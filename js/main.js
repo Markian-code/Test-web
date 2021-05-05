63c671d6aa5d657abffa00ff136b70084bb14063
Markian-code <markuskovalko@gmail.com>
2021-05-05 18:24:13 +0300

    mobile_version_is_ready

--- js/main.js
+++ js/main.js
@@ -4,8 +4,32 @@ $(function(){
     slidesToShow: 6,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-arrow slick-prevs"><img src="images/arrow-left.png" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-arrow slick-nexts"><img src="images/arrow-right.png" alt=""></button>'
-  });          
+    nextArrow: '<button type="button" class="slick-arrow slick-nexts"><img src="images/arrow-right.png" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1293,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 846,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 585,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });  
+  
+  $('.menu__btn').on('click', function() {
+    $('.menu__list').toggleClass('menu__list--active')
+  })
 });
 
 $('.splide__arrow--prev').hover(
